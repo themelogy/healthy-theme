139783f87809b20db08c052aab7794c50e339357
sagsoz06 <designer@visualturk.net>
2020-01-21 02:25:30 +0300

    Fixed embed url

--- views/modules/page/partials/video.blade.php
+++ views/modules/page/partials/video.blade.php
@@ -2,7 +2,7 @@
     <div class="card card-shadow position-relative" data-aos="flip-up" data-aos-duration="1200">
         <img class="card-img-top video-img" src="{!! $media->present()->embedImage(300,200,'fit',80) !!}" alt="wrappixel kit">
         <div class="embed-responsive hide embed-responsive-16by9" data-animation="animated fadeIn">
-            <iframe width="100%" height="315" src="{!! $media->video_link !!}" frameborder="0" allowfullscreen></iframe>
+            <iframe width="100%" height="315" src="{!! $media->embedurl !!}" frameborder="0" allowfullscreen></iframe>
         </div>
         <button type="button" class="btn btn-themecolor btn-circle btn-lg video-btn align-middle position-absolute" style="left:50%; top:50%; margin-left: -35px; margin-top:-35px;"><i class="fa fa-play"></i> </button>
     </div>
